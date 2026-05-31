c7edd4c529fd2e4298a25800c04c799e2e3f1882
TranQuoc2k6 <qatran2k6@gmail.com>
2026-06-01 04:56:33 +0700

    Add files via upload

--- b3.py
+++ b3.py
@@ -57,20 +57,50 @@ while True:
             if flag == 0:
                 input_product_name = input("- Nhập tên sản phẩm: ").strip().capitalize()
     
-                input_price = float(input("- Nhập giá sản phẩm: "))
+                input_price = int(input("- Nhập giá sản phẩm: "))
                 while input_price <= 0:
                     print("Giá sản phẩm phải là số dương")
-                    input_price = float(input("- Nhập giá sản phẩm: "))
+                    input_price = int(input("- Nhập giá sản phẩm: "))
 
                 input_quantity = int(input("- Nhập số lượng sản phẩm: "))
                 while input_quantity <= 0:
                     print("Số lượng sản phẩm phải là số dương")
                     input_quantity = int(input("- Nhập số lượng sản phẩm: "))
-
+                add_product["product_id"] = input_product_id
+                add_product["product_name"] = input_product_name
+                add_product["price"] = input_price
+                add_product["quantity"] = input_quantity
+                product_list.append(add_product)
+                print("Thêm sản phẩm thành công")
         case "3":
-            print()
+            input_update_product_id = input("Nhập mã sản phẩm cần cập nhật: ").strip().upper()
+            flag = 0
+            for emp in product_list:
+                if input_update_product_id == emp["product_id"]:
+                    flag = 1
+                    input_update_product_name = input("Nhập tên sản phẩm mới: ").strip().capitalize()
+                    input_update_price = int(input("Nhập giá sản phẩm mới: "))
+                    input_update_quantity = int(input("Nhập số lượng sản phẩm mới: "))
+                    emp["product_name"] = input_update_product_name
+                    emp["price"] = input_update_price
+                    emp["quantity"] = input_update_quantity
+                    print("Sửa thông tin sản phẩm thành công")
+            if flag == 0:
+                print("Không tìm thấy mã sản phẩm cần cập nhật!”")
+
         case "4":
-            print()
+            input_delete_product_id = input("Nhập mã sản phẩm cần xóa: ").strip().upper()
+            index = -1
+            flag = 0
+            for emp in product_list:
+                index += 1
+                if input_delete_product_id == emp["product_id"]:
+                    product_list.pop(index)
+                    flag = 1
+                    print("Đã xóa thành công")
+            if flag == 0:
+                print("Không tìm thấy mã sản phẩm cần xoá!")
+
         case "5":
             print("Thoát chương trình.Sau đó dừng chương trình.")
             break
